b7403a60889d63e756c86be3cdcdf5f9fc4c63ea
cswambold24 <cswambold24@blakeschool.org>
2024-01-24 13:32:52 -0600

    Initial Commit

---
+++ A5StringTestThing/A5StringTestThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  A5StringTestThingApp.swift
+//  A5StringTestThing
+//
+//  Created by Carver Wambold24 on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct A5StringTestThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A5StringTestThing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A5StringTestThing
+//
+//  Created by Carver Wambold24 on 1/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
