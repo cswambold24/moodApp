9cbe52129321f2bf54f484cc5e4681bdfac3a877
cswambold24 <cswambold24@blakeschool.org>
2024-02-13 10:59:49 -0600

    HomeView created as well as mood, emotion, input.

---
+++ A5StringTestThing/Views/MoodView.swift
@@ -0,0 +1,18 @@
+//
+//  MoodView.swift
+//  A5StringTestThing
+//
+//  Created by Carver Wambold24 on 2/13/24.
+//
+
+import SwiftUI
+
+struct MoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoodView()
+}
