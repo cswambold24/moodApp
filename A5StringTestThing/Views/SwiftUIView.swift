986519f23b901d6a820980edcfc5cb4a00470bb2
cswambold24 <cswambold24@blakeschool.org>
2024-02-26 10:35:47 -0600

    Project lmost finished, final touches left.

---
+++ A5StringTestThing/Views/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  A5StringTestThing
+//
+//  Created by Carver Wambold24 on 2/22/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
